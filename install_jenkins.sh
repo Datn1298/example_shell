271691465068382a72933359d434f1e529f501b6
Nguyen Trong Dat (FIM HN) <DatNT202@fpt.com.vn>
2022-05-16 08:49:52 +0700

    init

--- install_jenkins.sh
+++ install_jenkins.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
-#################################################
-#                                               #
-# A shell script to install Jenkins on CentOS   #
-#                                               #
-#################################################
 
-# check if the current user is root
+# kiem tra xem nguoi dung hien tai co phai nguoi dung root k
+# neu k => thoat
 if [[ $(/usr/bin/id -u) != "0" ]]; then
     echo -e "This looks like a 'non-root' user.\nPlease switch to 'root' and run the script again."
     exit
@@ -13,7 +9,7 @@ fi
 
 jvconfirm="y"
 
-# Function to install Java using Yum
+# cai java su dung yum
 install_java() {
     echo -e "\nIn install_java\n"
     yum update -y
@@ -23,10 +19,12 @@ install_java() {
     install_jenkins
 }
 
-# Function to install Jenkins using the Jenkins repo
+# cai jenkins
 install_jenkins() {
     echo -e "\nIn install_jenkins\n"
+# cai git
     yum install git -y
+# tai jenkins repo
     wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
     rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
     yum install jenkins -y
@@ -46,7 +44,9 @@ install_jenkins() {
     exit
 }
 
-# Check if Java is installed, and install Jenkins
+# kiem tra xem da cai java chua
+# neu da cai => goi ham install_jenkins
+# neu chua cai => goi ham install_java
 if $(java -version 2>&1 >/dev/null | grep 'version'); then
     echo -e "\nJava is installed, proceeding with Jenkins installation."
     install_jenkins
