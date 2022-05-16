8dd1c8ca23f76958402f1910055821712306dff8
Nguyen Trong Dat (FIM HN) <DatNT202@fpt.com.vn>
2022-05-16 08:43:48 +0700

    init

--- install_nextcloud.sh
+++ install_nextcloud.sh
@@ -1,13 +1,15 @@
-#Installation
+# cai apache2
 apt install apache2 -y;
 cd /var/www/; 
+# tai va giai nen file nextcloud
 wget https://download.nextcloud.com/server/releases/nextcloud-21.0.0.zip
 unzip nextcloud-21.0.0.zip;
 
-#Dependencies
+# cai dat cac thu vien kem theo
 apt install mariadb-server php7.4 php7.4-mysql php7.4-xml php7.4-zip php7.4-gd php7.4-curl php7.4-mbstring -y
 
-#Apache2 Configuration
+# cau hinh apache2
+
 sudo nano /etc/apache2/sites-available/nextcloud.conf
   <VirtualHost *:80>
 	ServerName nextcloud.example.com
@@ -22,9 +24,11 @@ sudo nano /etc/apache2/sites-available/nextcloud.conf
 	#Include conf-available/serve-cgi-bin.conf
   </VirtualHost>
 
-  
+# link 2 file  
 ln -s /etc/apache2/sites-available/nextcloud.conf /etc/apache2/sites-enabled/nextcloud.conf
 service apache2 restart
+
+# doi user:group
 chown -R www-data:www-data /var/www/nextcloud/
 
 #Database Configuration
