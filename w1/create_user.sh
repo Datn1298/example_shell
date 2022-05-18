7d352ebdf7fce4834b4608f3ca63a9d79cb394f0
Nguyen Trong Dat (FIM HN) <DatNT202@fpt.com.vn>
2022-05-18 16:31:52 +0700

    add: shell script

--- w1/create_user.sh
+++ w1/create_user.sh
@@ -5,21 +5,14 @@
 # output: user
 
 
-echo "Enter the User-name :"
+echo -n "Enter the User-name :"
 read create_user
 if [[ $(cat /etc/passwd | grep "$create_user") ]];
 then
 	echo "user $create_user already exist!"
 else
 	useradd $create_user
-	mkdir /home/$create_user/.ssh
-	chown $create_user:$create_user /home/$create_user/.ssh
-	chmod 700 /home/$create_user/.ssh
-	ssh-keygen -t rsa -N "" -f /home/$create_user/.ssh/id_rsa 2>/dev/null
-	chown $create_user:$create_user /home/$create_user/.ssh/id_rsa
-	chown $create_user:$create_user /home/$create_user/.ssh/id_rsa.pub
-	touch /home/$create_user/.ssh/authorized_keys
-	cp /home/$create_user/.ssh/id_rsa.pub /home/$create_user/.ssh/authorized_keys
-	chmod 600 /home/$create_user/.ssh/authorized_keys
-	chown $create_user:$create_user /home/$create_user/.ssh/authorized_keys
+	echo -n "Enter the password"
+	read password
+	echo "$password" | passwd --stdin $create_user
 fi
--- w1/install_nginx.sh
+++ w1/install_nginx.sh
@@ -24,16 +24,20 @@ echo "add_header X-XSS-Protection "1; mode=block"" >> nginx.conf
 
 # HTTP Strict Transport Security
 # add_header Strict-Transport-Security 'max-age=31536000; 
+echo "add_header Strict-Transport-Security 'max-age=31536000;" >> nginx.conf
 # restart lai nginx
 
 # X-Frame-Options: them vao file nginx.conf
 # add_header X-Frame-Options “DENY”;
+echo "add_header X-Frame-Options “DENY”;" >> nginx.conf
 # systemctl restart nginx.service
 
 # X-Content-Type-Options
 # add_header X-Content-Type-Options nosniff;
+echo "add_header X-Content-Type-Options nosniff;" >> nginx.conf
 # systemctl restart nginx.service
 
 # Content Security Policy
 # add_header Content-Security-Policy "default-src 'self'";
+echo "add_header Content-Security-Policy "default-src 'self'";" >> nginx.conf
 # systemctl restart nginx.service
