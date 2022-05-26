1d72acd68e329490240ea9dc90f0769547f810ec
Datn1298 <lfh275@gmail.com>
2022-05-26 09:05:54 +0700

    init

---
+++ w1/check_os.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+# kiem tra permission cua 1 file
+# input: ten 1 file
+# output: quyen cua file do
+
+echo "INFO KERNEL"
+cat /proc/version
+
+echo "FREE AND USED MEMORY"
+free -h
+
+echo ""
